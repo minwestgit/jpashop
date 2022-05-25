67fedfe71e0f81765d84cea339cadf632113564a
minwestgit <minwest61@gmail.com>
2022-05-26 00:21:20 +0900

    엔티티 클래스 개발

---
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    //JPA 스펙상 기본 생성자 필요해서 만들어둬야. protected로 설정하는 것이 그나마 안전
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+}
