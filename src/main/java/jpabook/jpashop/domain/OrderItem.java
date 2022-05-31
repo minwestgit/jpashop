98a4f17bd252b0a5a4e5d1d8c73c5226f61cb203
minwestgit <minwest61@gmail.com>
2022-05-31 22:17:48 +0900

    주문 관련 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,13 +1,16 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.Item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) //OrderItem 인스턴스 생성시 createOrderItem 메서드로 생성하는 것 외에는 막기 위해 protected 걸어줌
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -23,6 +26,25 @@ public class OrderItem {
     private Order order;
 
     private int orderPrice; //주문 가격
-
     private int count; //주문 수량
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
